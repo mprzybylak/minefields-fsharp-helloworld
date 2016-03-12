4c09832bdb17868edd54abe2d8bba1c666e09293
Michał Przybylak <michal.przybylak@gmail.com>
2016-03-12 22:32:08 +0100

    lists, ranges, arrays tryout

--- Fsharp Hello World/Fsharp Hello World/Program.fs
+++ Fsharp Hello World/Fsharp Hello World/Program.fs
@@ -19,10 +19,18 @@ let sub(x:int32, y:int32) =     // function definition with type annotation (dos
 let operation a b oper = 
     oper a b
 
+let simpleList = [1;2;3]
+let simpleRange = [1..10]
+let simpleArray = [|1;2;3|]
+
 [<EntryPoint>]
 let main argv = 
     printfn "hello world %d %d %s %d 3+2=%i 3-2=%d custom operation 3+3=%d" lucky unlucky duplicate mutableValue (add 3 2) (sub(3,2)) (operation 3 3 add)
     printfn "lambda add 3+4 = %d" (operation 3 4 (fun x y->x+y))
+    printfn "list: %A list from range: %A, range and map call: %A" simpleList simpleRange (List.map (fun x->x*2) simpleList)
+    printfn "many calls to List module functions: %A" (List.map (fun x->x*2) (List.filter (fun x -> x % 2 = 0) simpleRange))
+    printfn "many calls to List module with foward pipe operator: %A" (simpleRange |> List.filter (fun x -> x % 2 = 0) |> List.map (fun x -> x *2))
+    printfn "simple array: %A" simpleArray
     System.Console.ReadLine() |> ignore
     0 // return an integer exit code
 
