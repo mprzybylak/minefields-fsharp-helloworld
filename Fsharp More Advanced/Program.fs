bcf49efd94d697cb1a323a5bf97b750381a3fb2c
Michał Przybylak <michal.przybylak@gmail.com>
2016-03-13 17:16:51 +0100

    simple operators

--- Fsharp More Advanced/Program.fs
+++ Fsharp More Advanced/Program.fs
@@ -12,11 +12,23 @@ let sub x y = x - y
 let swapargs f x y = f y x
 let dec = swapargs sub 1
 
+let (><) x y = x * y    // custom operator example
+
+// operator within type
+type Point(x:int32, y:int32) = 
+    member this.x = x;
+    member this.y = y;
+    static member (+)(p1:Point, p2:Point) = new Point(p1.x + p2.x, p1.y + p2.y)
+
+let p1,p2 = new Point(2,2), new Point(3,4)
+let p3 = p1 + p2
 
 [<EntryPoint>]
 let main argv = 
     printfn "inc 10 = %i" (inc 10)
     printfn "dec 10 = %i" (dec 10)
+    printfn "10><2=%d" (10><2)
+    printfn "x=2,y=2 + x=3,y=4 = x=%i,y=%i" p3.x p3.y
     System.Console.ReadLine() |> ignore
     0 // return an integer exit code
 
